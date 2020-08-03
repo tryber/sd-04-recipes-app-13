b851a83ecde4f37d346da05eb7b739b4d9bfe979
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-08-03 15:44:33 -0300

    foi

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,13 +8,19 @@ function Footer() {
   return (
     <footer>
       <div>
-        <Link to="/bebidas" data-testid="drinks-bottom-btn"><img src={drinkIcon} alt="Drink path" /></Link>
+        <Link to="/bebidas" data-testid="drinks-bottom-btn">
+          <img src={drinkIcon} alt="Drink path" />
+        </Link>
       </div>
       <div>
-        <Link to="/explorar" data-testid="explore-bottom-btn"><img src={exploreIcon} alt="Explore path" /></Link>
+        <Link to="/explorar" data-testid="explore-bottom-btn">
+          <img src={exploreIcon} alt="Explore path" />
+        </Link>
       </div>
       <div>
-        <Link to="/comidas" data-testid="food-bottom-btn"><img src={mealIcon} alt="Meal path" /></Link>
+        <Link to="/comidas" data-testid="food-bottom-btn">
+          <img src={mealIcon} alt="Meal path" />
+        </Link>
       </div>
     </footer>
   );
