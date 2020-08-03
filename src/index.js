5cb2cf478628fcace780a00564d9469071ec39e1
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-08-03 15:53:36 -0300

    .

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <RecipeContext.Consumer>
+    <RecipeContext>
       <App />
-    </RecipeContext.Consumer>
+    </RecipeContext>
   </BrowserRouter>,
-  document.getElementById('root'));
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
