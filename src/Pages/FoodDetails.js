e07c417f598e9ebe9eef7b161e90f348043e8706
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-08-04 19:17:33 -0300

    chorar no cc

--- src/Pages/FoodDetails.js
+++ src/Pages/FoodDetails.js
@@ -1,6 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { RecipeContext } from '../context/index';
+import ReactPlayer from 'react-player';
 import { getById } from '../services/foodApi';
+import FoodAndDrinkCard from '../components/FoodAndDrinkCard';
+import { getByName } from '../services/drinkApi';
+
+async function alternateDrinks() {
+  const Drinks = await getByName('').then((resp) => resp);
+  console.log(Drinks)
+  return (
+    <div>
+      <h1>Recomended</h1>
+      <FoodAndDrinkCard data={Drinks} info="drink" slice="6" test="recomendation" />
+    </div>
+  );
+}
 
 function FoodDetails() {
   const [food, setFood] = useState('');
@@ -14,17 +27,30 @@ function FoodDetails() {
 
   return (
     <div>
-      <img src={food.strMealThumb} alt="food-img" />
-      <h1 className="food-title">{food.strMeal}</h1>
-      <h4>{food.strCategory}</h4>
+      <img data-testid="recipe-photo" src={food.strMealThumb} alt="food-img" />
+      <h1 data-testid="recipe-title" className="food-title">{food.strMeal}</h1>
+      <h4 data-testid="recipe-category">{food.strCategory}</h4>
       <div>
-        <h1>Ingredientes</h1>
+        <h1>Ingredients</h1>
         <ul>
           {Object.keys(food).map((ing, index) => (
-            <li>{food[`${ing}${index}`]}</li>
+            (food[`strIngredient${index + 1}`]) && (
+              <li key={ing} data-testid={`${index}-ingredient-name-and-measure`}>
+                {`${food[`strIngredient${index + 1}`]} - ${food[`strMeasure${index + 1}`]}`}
+              </li>
+            )
           ))}
         </ul>
       </div>
+      <div>
+        <h1>Instructions</h1>
+        <p data-testid="instructions">{food.strInstructions}</p>
+      </div>
+      <div>
+        <h1>Video</h1>
+        <ReactPlayer url={food.strYoutube} data-testid="video" />
+      </div>
+      <div>{() => alternateDrinks()}</div>
     </div>
   );
 }
--- src/components/FoodAndDrinkCard.js
+++ src/components/FoodAndDrinkCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const FoodAndDrinkCard = ({ data, info }) => {
+const FoodAndDrinkCard = ({ data, info, slice = 12, test = 'recipe' }) => {
   if (info === 'food') {
     return (
       <div className="card-general-container">
@@ -10,11 +10,11 @@ const FoodAndDrinkCard = ({ data, info }) => {
           <div className="card-container">
             <Link
               to={`/comidas/${food.idMeal}`}
-              data-testid={`${index}-recipe-card`}
+              data-testid={`${index}-${test}-card`}
               key={food.idMeal}
             >
-              <img className="card-img" src={food.strMealThumb} alt={food.strMeal} width={'60px'} />
               <h3 className="card-name">{food.strMeal}</h3>
+              <img className="card-img" src={food.strMealThumb} alt={food.strMeal} width={'60px'} />
             </Link>
           </div>
         ))}
@@ -23,11 +23,11 @@ const FoodAndDrinkCard = ({ data, info }) => {
   }
   return (
     <div className="card-general-container">
-      {data.slice(0, 12).map((drink, index) => (
+      {data.slice(0, Number(slice)).map((drink, index) => (
         <div className="card-container">
           <Link
             to={`/bebidas/${drink.idDrink}`}
-            data-testid={`${index}-recipe-card`}
+            data-testid={`${index}-r${test}-card`}
             key={drink.idDrink}
           >
             <h3 className="card-name">{drink.strDrink}</h3>
@@ -42,6 +42,8 @@ const FoodAndDrinkCard = ({ data, info }) => {
 FoodAndDrinkCard.propTypes = {
   data: PropTypes.arrayOf(PropTypes.object).isRequired,
   info: PropTypes.string.isRequired,
+  slice: PropTypes.string.isRequired,
+  test: PropTypes.string.isRequired,
 };
 
 export default FoodAndDrinkCard;
