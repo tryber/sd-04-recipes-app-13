29d8a2a7fb0bfd89c663ab1fec30ec50386686f1
igorgbr <igorgphotoarte@gmail.com>
2020-08-06 18:20:48 -0300

    nao aprova ainda

--- src/Pages/ExploreDrinks.js
+++ src/Pages/ExploreDrinks.js
@@ -1,13 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
+import Header from '../components/Header/Header';
+import '../styles/ExploreScreen.css';
 
-function ExploreDrinks() {
+function ExploreFoods() {
   return (
     <div>
-      <h1>oi</h1>
+      <Header />
+      <Link to="/explorar/comidas">
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        className="btn-explore-by-ingredient"
+      >
+        Por Ingredientes
+      </button>
+      </Link>
+      <button
+        type="button"
+        data-testid="explore-by-area"
+        className="btn-explore-by-area"
+      >
+        Por Local de Origem
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        className="btn-explore-surprise"
+      >
+        Me Surpreenda!
+      </button>
       <Footer />
     </div>
   );
 }
 
-export default ExploreDrinks;
+export default ExploreFoods;
--- src/Pages/ExploreFoods.js
+++ src/Pages/ExploreFoods.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
+import Header from '../components/Header/Header';
+import '../styles/ExploreScreen.css';
 
 function ExploreFoods() {
   return (
     <div>
-      <h1>oi</h1>
+      <Header />
+      <Link to="/explorar/comidas">
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        className="btn-explore-by-ingredient"
+      >
+        Por Ingredientes
+      </button>
+      </Link>
+      <button
+        type="button"
+        data-testid="explore-by-area"
+        className="btn-explore-by-area"
+      >
+        Por Local de Origem
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        className="btn-explore-surprise"
+      >
+        Me Surpreenda!
+      </button>
       <Footer />
     </div>
   );
