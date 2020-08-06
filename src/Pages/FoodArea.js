9401ffa19aac8e23b6365988b58272a5b00b1ffd
henrique <hbaetaleite@gmail.com>
2020-08-06 19:41:24 -0300

    codeClimate v3

--- src/Pages/FoodArea.js
+++ src/Pages/FoodArea.js
@@ -4,30 +4,35 @@ import Header from '../components/Header/Header';
 import { getAllFoodByArea } from '../services/foodApi';
 
 function FoodArea() {
-  const [dropDown, setDropDown] = useState('');
+  const [dropDown, setDropDown] = useState([]);
+  const [dropDownChoosen, setDropDownChoosen] = useState('');
 
   useEffect(() => {
     getAllFoodByArea().then((area) => setDropDown(area));
   }, []);
 
   const isDropDown = !!dropDown;
-  const counter = 2;
+
+  const handleSelect = (e) => {
+    setDropDownChoosen(e.target.value);
+  };
 
   return (
     <div>
       <Header title="Explorar Origem" search />
-      <select data-testid="explore-by-area-dropdown">
+      <select onChange={(e) => handleSelect(e)} data-testid="explore-by-area-dropdown">
         <option>All</option>
         {isDropDown &&
           dropDown.map((area, index) => (
             <option
               data-testid={`${area.strArea}-option`}
-              key={`dropDown-${counter + 1}-${area}`}
+              key={`dropDown-${index + 1}-${area}`}
             >
               {area.strArea}
             </option>
           ))}
       </select>
+      <h2>{dropDownChoosen}</h2>
       <Footer />
     </div>
   );
