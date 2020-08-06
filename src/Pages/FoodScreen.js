89bc2dc883384bf62f13929b9ccf7ed72473c8ec
henrique <hbaetaleite@gmail.com>
2020-08-06 15:13:46 -0300

    codeclimate v5

--- src/Pages/FoodScreen.js
+++ src/Pages/FoodScreen.js
@@ -21,8 +21,9 @@ const FoodScreen = () => {
       setCategories(Datacategories);
     });
   }, [name]);
-  const changeCategory = (strCategory) =>
-    (name === strCategory ? setName('') : setName(strCategory));
+  const changeCategory = (strCategory) => (
+    name === strCategory ? setName('') : setName(strCategory)
+  );
   if (!data) {
     return (
       <div>
