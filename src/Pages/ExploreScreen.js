d4866ec94722086681716f84dc7201cc14512e8b
igorgbr <igorgphotoarte@gmail.com>
2020-08-06 17:22:07 -0300

    tela explorar

--- src/Pages/ExploreScreen.js
+++ src/Pages/ExploreScreen.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
+import Header from '../components/Header/Header';
 
 function ExploreScreen() {
   return (
     <div>
-      <h1>oi</h1>
+      <Header />
+      <Link to="/explorar/comidas">
+        <button
+          type="button"
+          data-testid="explore-food"
+          className="btn-explore-food"
+        >
+          Explorar Comidas
+        </button>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <button
+          type="button"
+          data-testid="explore-drinks"
+          className="btn-explore-drinks"
+        >
+          Explorar Bebidas
+        </button>
+      </Link>
       <Footer />
     </div>
   );
