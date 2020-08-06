a400336057838b88377738d32a10c52db91dfd5d
henrique <hbaetaleite@gmail.com>
2020-08-06 15:16:36 -0300

    codeclimate v7

--- src/components/utils/Input.js
+++ src/components/utils/Input.js
@@ -1,27 +1,5 @@
 import React from 'react';
 
-<<<<<<< HEAD
-const renderInput = (datatest, type, value, onChange, label, name) => (
-  <div>
-    <input
-      onChange={(e) => onChange(e.target.value)}
-      data-testid={datatest}
-      type={type}
-      value={value}
-=======
 const RenderInput = ({ ...props }) => <input {...props} />;
 
 export default RenderInput;
-
-/*
->>>>>>> a5de0d46ca75e21e0fa1f6886a3bdc34e8963459
-      required
-      size="30"
- */
-
-// onChange={(e) => onChange(e.target.value)}
-// data-testid={datatest}
-// value={value}
-// className={`input-login-${type}`}
-// placeholder={label}
-// name={name}
