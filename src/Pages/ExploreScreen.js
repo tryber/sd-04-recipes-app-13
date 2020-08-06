3027b74b4275845a1a7dbf914ec5529f1e385940
igorgbr <igorgphotoarte@gmail.com>
2020-08-06 17:39:39 -0300

    tela explorar CSS

--- src/Pages/ExploreScreen.js
+++ src/Pages/ExploreScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header/Header';
+import '../styles/ExploreScreen.css';
 
 function ExploreScreen() {
   return (
