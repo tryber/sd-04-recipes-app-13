c229a5f3e34ad565ebe8716e22f749d3ae78734c
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-08-09 20:54:36 -0300

    finalmente liberado

--- src/components/utils/RecipeButton.js
+++ src/components/utils/RecipeButton.js
@@ -5,15 +5,17 @@ import { loadStorage } from '../../services/localStorage';
 
 function RecipeButton({ type, recipe }) {
   const [doneRecipe, setDoneRecipe] = useState([]);
-  const [doingRecipe, setDoingRecipe] = useState([]);
+  const [doingRecipe, setDoingRecipe] = useState({});
   const [message, setMessage] = useState('Iniciar Receita');
   const [visible, setVisible] = useState(true);
 
   const searchtypebutton = () => {
     console.log(doingRecipe);
+    const isDoingArray = doingRecipe[type] || [];
     const isDone = doneRecipe
       .filter((done) => done.id === (recipe.idMeal || recipe.idDrink));
-    const isDoing = doingRecipe[type]
+
+    const isDoing = isDoingArray
       .filter((doing) => doing === (recipe.idMeal || recipe.idDrink)) || [];
 
     if (isDone.lengh > 0) setVisible(false);
@@ -22,7 +24,7 @@ function RecipeButton({ type, recipe }) {
 
   useEffect(() => {
     setDoneRecipe(JSON.parse(loadStorage('doneRecipes')) || []);
-    setDoingRecipe(JSON.parse(loadStorage('inProgressRecipes')) || ['cocktails', 'meals']);
+    setDoingRecipe(JSON.parse(loadStorage('inProgressRecipes')) || { cocktails: [], meals: [] });
     searchtypebutton();
   }, []);
 
