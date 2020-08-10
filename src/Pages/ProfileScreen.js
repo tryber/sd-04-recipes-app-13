5e65233def9f2b0cb02bc23b772d0f7c43d50b49
henrique <hbaetaleite@gmail.com>
2020-08-10 17:59:44 -0300

    correcoes data-testid

--- src/Pages/ProfileScreen.js
+++ src/Pages/ProfileScreen.js
@@ -4,26 +4,34 @@ import Header from '../components/Header/Header';
 import Footer from '../components/Footer';
 import { loadStorage } from '../services/localStorage';
 
-
 function ProfileScreen() {
   const cleanAndBack = () => {
     localStorage.clear();
   };
 
+  const searchUser = loadStorage('user');
+
   return (
     <Fragment>
-      {console.log(loadStorage('user'))}
       <Header title="Perfil" />
       <div>
-        <h2 data-testid="profile-email">{loadStorage('user').email}</h2>
+        <h2 data-testid="profile-email">{searchUser ? searchUser.email : ''}</h2>
         <Link to="/receitas-favoritas">
-          <button type="button" data-testid="profile-favorite-btn">Receitas Favoritas</button>
+          <button type="button" data-testid="profile-favorite-btn">
+            Receitas Favoritas
+          </button>
         </Link>
         <Link to="/receitas-feitas">
-          <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
+          <button type="button" data-testid="profile-done-btn">
+            Receitas Feitas
+          </button>
         </Link>
         <Link to="/">
-          <button type="button" data-testid="profile-logout-btn" onClick={() => cleanAndBack()}>
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+            onClick={() => cleanAndBack()}
+          >
             Sair
           </button>
         </Link>
