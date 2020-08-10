c5e25c0a49fe00c71dc4e2fb06a10e7f0bbfa8d8
AliceAAtalla <aliceatalla36@gmail.com>
2020-08-10 15:08:17 -0300

    Nao funcionando ainda

--- src/Pages/DrinkProgress.js
+++ src/Pages/DrinkProgress.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { getById } from '../services/foodApi';
+import { getById } from '../services/drinkApi';
 import HeaderDetails from '../components/HeaderDetails';
+import RenderInput from '../components/utils/Input';
 
 function DrinkProgress() {
   const [recipe, setRecipe] = useState('');
@@ -14,6 +15,27 @@ function DrinkProgress() {
   return (
     <div>
       <HeaderDetails recipe={recipe} />
+      <div>
+        <h1>Ingredientes</h1>
+        {Object.keys(recipe).map(
+          (ingredient, index) => recipe[`strIngredient${index + 1}`] && (
+            <div>
+              <label htmlFor={ingredient}>
+                <RenderInput
+                  type="checkbox"
+                  id={ingredient}
+                  value={ingredient}
+                  key={ingredient}
+                  data-testid={`${index}-ingredient-step`}
+                />
+                {`${recipe[`strIngredient${index + 1}`]} - ${
+                  recipe[`strMeasure${index + 1}`]
+                }`}
+              </label>
+            </div>
+          ),
+        )}
+      </div>
     </div>
   );
 }
--- src/Pages/FoodProgress.js
+++ src/Pages/FoodProgress.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { getById } from '../services/foodApi';
 import HeaderDetails from '../components/HeaderDetails';
+import RenderInput from '../components/utils/Input';
 
 function FoodProgress() {
   const [recipe, setRecipe] = useState('');
+  const [completed, setCompleted] = useState('');
 
   useEffect(() => {
     const pathName = window.location.pathname.slice(9);
@@ -11,9 +13,48 @@ function FoodProgress() {
     getById(id).then((data) => setRecipe(data[0]));
   }, []);
 
+  // useEffect(() => {
+  //   setCompleted(prevState => [
+  //     ...prevState,
+  //     {
+  //       isCompleted}
+  //   ]);
+  // }, [recipe]);
+
+  const completedStep = (index, recipe) => {
+    // e.target.checked === true ? setCompleted(true) : setCompleted(false);
+    
+  };
+
   return (
     <div>
       <HeaderDetails recipe={recipe} foods />
+      <div>
+        <h1>Ingredientes</h1>
+        {Object.keys(recipe).map(
+          (ingredient, index) =>
+            recipe[`strIngredient${index + 1}`] && (
+              <div>
+                <RenderInput
+                  type="checkbox"
+                  id={ingredient}
+                  value={ingredient}
+                  key={ingredient}
+                  data-testid={`${index}-ingredient-step`}
+                  onClick={() => completedStep(index, recipe)}
+                />
+                <label
+                  htmlFor={ingredient}
+                  style={{ textDecoration: completed ? 'line-through' : '' }}
+                >
+                  {`${recipe[`strIngredient${index + 1}`]} - ${
+                    recipe[`strMeasure${index + 1}`]
+                  }`}
+                </label>
+              </div>
+            ),
+        )}
+      </div>
     </div>
   );
 }
