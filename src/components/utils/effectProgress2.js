7fb76582fa9b65655fd4974fed4155775c91616e
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-08-12 14:16:52 -0300

    href da alice

--- src/components/utils/effectProgress2.js
+++ src/components/utils/effectProgress2.js
@@ -2,7 +2,8 @@ const effectProgress2 = (getById, setRecipe, setPath) => {
   const pathName = window.location.pathname.slice(9);
   const id = pathName.replace(/\D/g, '');
   getById(id).then((data) => setRecipe(data[0]));
-  setPath(window.location.href.slice(0, 35));
+  const pathHref = window.location.href.replace('/in-progress', '');
+  setPath(pathHref);
 };
 
 export default effectProgress2;
