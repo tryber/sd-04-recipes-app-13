a5f12d4323926519e93f14bf412c7f1aa1d5ec61
henrique <hbaetaleite@gmail.com>
2020-08-12 16:50:50 -0300

    codeClimate v6

--- src/Pages/DrinkScreen.js
+++ src/Pages/DrinkScreen.js
@@ -26,13 +26,7 @@ const DrinkScreen = () => {
     console.log('Procopio Rules');
     return name === strCategory ? setName('') : setName(strCategory);
   };
-  if (!data) {
-    return alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
-      // <div>
-      //   <Header title="Bebidas" search />
-      //   <Footer />
-      // </div>
-  };
+  if (!data) return alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
   if (data.length === 0) return <Loading />;
   return (
     <div className="general-container">
