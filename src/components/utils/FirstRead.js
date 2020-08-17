548acb6f0816ab6debc46ad96f648e87d0c672a6
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-08-17 19:45:52 -0300

    commit pra uso do projeto

--- src/components/utils/FirstRead.js
+++ src/components/utils/FirstRead.js
@@ -1,28 +1,13 @@
 import { loadStorage } from '../../services/localStorage';
 
-const firstReadMeal = (loadIngStorage, recipeId) => {
-  console.log('entrou no Meal');
-  const ingredientKey = Object.keys(loadIngStorage.meals).filter((meal) => meal === recipeId);
-  return loadIngStorage.meals[ingredientKey];
-};
-
-const firstReadDrink = (loadIngStorage, recipeId) => {
-  console.log('entrou no Drink');
-  const ingredientKey = Object.keys(loadIngStorage.cocktails).filter((drink) => (
-    drink === recipeId));
-  return loadIngStorage.cocktails[ingredientKey];
-};
-
 const firstRead = (setIngredients, ingredients, recipe, setReadStorage, type) => {
   const recipeId = recipe.idMeal || recipe.idDrink;
   const loadIngStorage = JSON.parse(loadStorage('inProgressRecipes')) || [];
+  const loadMealStorage = loadIngStorage.meals;
+  const loadDrinkStorage = loadIngStorage.cocktails;
   const newIngredients = [...ingredients];
-  let usedIngredients = [];
   console.log(type);
-  usedIngredients = (type === 'meal')
-    ? firstReadMeal(loadIngStorage, recipeId)
-    : firstReadDrink(loadIngStorage, recipeId);
-  console.log(loadIngStorage);
+  const usedIngredients = loadMealStorage[recipeId] || loadDrinkStorage[recipeId] || [];
   if (usedIngredients && usedIngredients.length > 0) {
     usedIngredients.map((numb) => {
       newIngredients[numb].isCompleted = true;
